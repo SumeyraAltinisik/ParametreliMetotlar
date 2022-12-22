7a7749697d048c99693332ea7eda0760d04c5147
SumeyraAltinisik <sfa2023@gmail.com>
2022-12-22 15:21:04 +0300

    Add project files.

---
+++ ParametreliMetotlar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametreliMetotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Kısa kenarı giriniz:");
+            int kisaKenar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Uzun kenarı giriniz:");
+            int uzunKenar = Convert.ToInt32(Console.ReadLine());
+
+            CevreHesapla(kisaKenar, uzunKenar);
+            int cevap = AlanHesapla(kisaKenar, uzunKenar);
+            Console.WriteLine("Alanı:{0}", cevap);
+
+            Console.ReadKey();
+        }
+       
+        static void CevreHesapla(int kisaKenar, int uzunKenar)
+        {
+            int cevre = 2 * (kisaKenar + uzunKenar);
+            Console.WriteLine("Çevresi:{0}", cevre);
+        }
+
+        static int AlanHesapla(int a, int b)
+        {
+            int alan = a * b;
+            return alan;
+        }
+    }
+}
